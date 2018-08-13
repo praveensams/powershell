f31a94302324ab70ed10304ccc929bc8b0289f75
praveensams <praveensams16@gmail.com>
2018-08-13 11:03:29 +0300

    uploadss

--- play.ps1
+++ play.ps1
@@ -1,1 +1,10 @@
-echo "hiii  sam in home" > c:\inetpub\wwwroot\index.html
+
+$acctKey = ConvertTo-SecureString -String "DAAppNg0uJr6/nH6ko5eKlk62hs5zSOBvLTUo1CrvcZrDmZv6F2shWTXAFI2XdX/2E1TLQ2Du57oM2z5kOYgEQ==" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList "Azure\tobedeletediag433", $acctKey
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\tobedeletediag433.file.core.windows.net\codestorage" -Credential $credential -Persist
+
+Start-Sleep -Seconds 2
+
+dir z:\  > c:\inetpub\wwwroot\index.html
+
+echo "hiii" >> c:\inetpub\wwwroot\index.html
